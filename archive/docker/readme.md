2c0ba6a72a382dfd3483c977a04218ac68572598
Matthieu Touré <63797411+iamgoats@users.noreply.github.com>
2024-12-20 09:23:48 +0100

    updated base file

---
+++ archive/docker/readme.md
@@ -0,0 +1,14 @@
+# Installation
+```
+sudo apt install docker docker-compose
+```
+
+# Lancement
+```
+docker-compose up
+```
+
+# Affichage
+- Ouvrir un navigateur à l'adresse http://localhost:3000
+- Id/Mdp : admin/admin
+- Trouver le dashboard dans la liste des dashboards
--- archive/drv_api.h
+++ archive/drv_api.h
@@ -21,9 +21,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define DRV_SUCCESS         0
-#define DRV_ERROR           -1
-#define DRV_VER_MISMATCH    -2
+#define DRV_SUCCESS         (0)
+#define DRV_ERROR           (-1)
+#define DRV_VER_MISMATCH    (-2)
 
 #define SER_MAX_FRAME_SIZE          (2)
 #define DRV_MAX_FRAMES              (16)
@@ -37,52 +37,52 @@ typedef struct {
 } serial_frame_t;
 
 /**
- * \brief Open connection with driver, and returns file descriptor of channel
+ * \brief   Open connection with driver, and returns file descriptor of channel
  * \param   void
- * \return int32_t : File descriptor (positive value), error otherwise :
- *                      - DRV_ERROR if an error occurs (is driver started ?)
- *                      - DRV_VER_MISMATCH if version of API is not compatible with driver
+ * \return  int32_t : File descriptor (positive value), error otherwise :
+ *              - DRV_ERROR if an error occurs (is driver started ?)
+ *              - DRV_VER_MISMATCH if version of API is not compatible with driver
  */
 int32_t drv_open(void);
 
 /**
  * \brief   Read UDP 100ms frame (blocking call)
  * \details Wait for next UDP 100ms frame and returns it
- * \param       drvFd       : File descriptor of channel with driver (get with open)
- * \param[out]  udpFrame    : Structure to fill with udp frame
- * \return int32_t : DRV_SUCCESS, or DRV_ERROR if an error occurs
+ * \param       drvFd : File descriptor of channel with driver (get with open)
+ * \param[out]  udpFrame : Structure to fill with udp frame
+ * \return  int32_t : DRV_SUCCESS, or DRV_ERROR if an error occurs
  */
 int32_t drv_read_udp_100ms(int32_t drvFd, uint8_t udpFrame[DRV_UDP_100MS_FRAME_SIZE]);
 
 /**
  * \brief   Write UDP 200ms frame
- * \param       drvFd       : File descriptor of channel with driver (get with open)
- * \param       udpFrame    : Structure to write
- * \return int32_t : DRV_SUCCESS, or DRV_ERROR if an error occurs
+ * \param       drvFd : File descriptor of channel with driver (get with open)
+ * \param       udpFrame : Structure to write
+ * \return  int32_t : DRV_SUCCESS, or DRV_ERROR if an error occurs
  */
-int32_t drv_write_udp_200ms(int32_t drvFd, uint8_t udpFrame[DRV_UDP_200MS_FRAME_SIZE]);
+int32_t drv_write_udp_200ms(int32_t drvFd, const uint8_t udpFrame[DRV_UDP_200MS_FRAME_SIZE]);
 
 /**
- * \brief Give all serial message received since the last call
- * \param       drvFd           : File descriptor of channel with driver (get with open)
- * \param[out]  serialData      : Structure to fill with read messages
- * \param[out]  serialDataLen   : Number of element in serialData structure
- * \return int32_t : DRV_SUCCESS, or DRV_ERROR if an error occurs
+ * \brief   Give all serial message received since the last call
+ * \param       drvFd : File descriptor of channel with driver (get with open)
+ * \param[out]  serialData : Structure to fill with read messages
+ * \param[out]  serialDataLen : Number of element in serialData structure
+ * \return  int32_t : DRV_SUCCESS, or DRV_ERROR if an error occurs
  */
 int32_t drv_read_ser(int32_t drvFd, serial_frame_t serialData[DRV_MAX_FRAMES], uint32_t* serialDataLen);
 
 /**
  * \brief Write data on serial lines
- * \param   drvFd           : File descriptor of channel with driver (get with open)
- * \param   serialData      : Pointer on structure containing messages to write on lines
- * \param   serialDataLen   : Number of element in serialData structure
+ * \param   drvFd : File descriptor of channel with driver (get with open)
+ * \param   serialData : Pointer on struct containing messages to write
+ * \param   serialDataLen : Number of element in serialData structure
  * \return int32_t : DRV_SUCCESS, or DRV_ERROR if an error occurs
  */
-int32_t drv_write_ser(int32_t drvFd, serial_frame_t* serialData, uint32_t serialDataLen);
+int32_t drv_write_ser(int32_t drvFd, const serial_frame_t* serialData, uint32_t serialDataLen);
 
 /**
  * \brief Close connection with driver
- * \param   drvFd           : File descriptor of channel with driver (get with open)
+ * \param   drvFd : File descriptor of channel with driver (get with open)
  * \return int32_t : DRV_SUCCESS, or DRV_ERROR if an error occurs
  */
 int32_t drv_close(int32_t drvFd);
